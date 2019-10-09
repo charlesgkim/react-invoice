bfeb6f3703f928d61a5c7331918df46a739a113a
Charles Guanglim Kim <charles.g.kim@gmail.com>
2019-10-09 08:08:17 +0100

    customer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './App.module.scss'
 import Invoice from './Invoice/Invoice'
+import Customer from './Customers/Customer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Invoice />
+        <Customer />
       </div>
     )
   }
